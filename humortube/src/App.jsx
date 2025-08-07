140a76499d2f5f6b3804c0dd75141a95763c6a93
studentKim2000 <166790301+studentKim2000@users.noreply.github.com>
2025-08-07 16:39:10 +0900

    유머튜브 완성

---
+++ humortube/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+
+  return (    
+    <>
+      <div className="center_div">
+        <h1>웃긴 유튜브 영상 모음</h1>
+
+        <h2>태권도</h2>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/SjEc9UarsUE?si=e7K8bYjH077B4-vT&amp;start=11" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/7zi_SoYdQhg?si=F8B-lqoAPQJq0bhf&amp;start=11" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        
+        <h2>만들기</h2>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/w2kt1Hr4i08?si=6CIs_Lno8hY0_HSW&amp;start=2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+    </>
+  )
+}
+
+export default App
